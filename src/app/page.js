204ec1afcb217e015a0958c10c3254435dfda498
BB <sorawitthassadorn15@gmail.com>
2025-08-18 04:06:29 +0700

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,103 +1,123 @@
-import Image from "next/image";
+'use client';
+import Image from 'next/image';
+import { useEffect, useState } from 'react';
+import { FullScreen, useFullScreenHandle } from 'react-full-screen';
 
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const [time, setTime] = useState(new Date());
+  const [active, setActive] = useState(false);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+  const handle = useFullScreenHandle();
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+  useEffect(() => {
+    if (handle.active) {
+      console.log('avtive');
+      setActive(true);
+    } else {
+      setActive(false);
+    }
+  }, [handle]);
+
+  const iconFull = () => {
+    return (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width={24}
+        height={24}
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth={2}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        className="icon icon-tabler icons-tabler-outline icon-tabler-arrows-maximize"
+      >
+        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+        <path d="M16 4l4 0l0 4" />
+        <path d="M14 10l6 -6" />
+        <path d="M8 20l-4 0l0 -4" />
+        <path d="M4 20l6 -6" />
+        <path d="M16 20l4 0l0 -4" />
+        <path d="M14 14l6 6" />
+        <path d="M8 4l-4 0l0 4" />
+        <path d="M4 4l6 6" />
+      </svg>
+    );
+  };
+
+  const iconNotFull = () => {
+    return (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width={24}
+        height={24}
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth={2}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        className="icon icon-tabler icons-tabler-outline icon-tabler-arrows-minimize"
+      >
+        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+        <path d="M5 9l4 0l0 -4" />
+        <path d="M3 3l6 6" />
+        <path d="M5 15l4 0l0 4" />
+        <path d="M3 21l6 -6" />
+        <path d="M19 9l-4 0l0 -4" />
+        <path d="M15 9l6 -6" />
+        <path d="M19 15l-4 0l0 4" />
+        <path d="M15 15l6 6" />
+      </svg>
+    );
+  };
+  return (
+    <FullScreen handle={handle}>
+      <div className="bg-primary font-cute flex h-screen w-screen flex-col items-center justify-center border font-[400]">
+        <section className="flex h-full flex-col items-center justify-center gap-10">
+          <div>
+            <span className="text-7xl font-bold text-white md:text-9xl lg:text-[300px]">
+              {time.toLocaleTimeString('th-TH', {
+                timeZone: 'Asia/Bangkok',
+                hour: '2-digit',
+                minute: '2-digit',
+                hour12: false,
+              })}
+            </span>
+            <span className="ml-4 text-3xl font-bold text-red-600 md:text-4xl lg:text-[100px]">
+              {time.toLocaleTimeString('th-TH', {
+                timeZone: 'Asia/Bangkok',
+                second: '2-digit',
+              })}
+            </span>
+          </div>
+          <div className='text-white text-3xl'>
+            {time.getDate()} / {time.getMonth()} / {time.getFullYear()}
+          </div>
+          {active ? (
+            <section
+              className="m-10 rounded-xl bg-white p-2 text-lg transition-colors duration-500 hover:cursor-pointer hover:bg-slate-500 hover:text-white md:text-xl lg:text-3xl"
+              onClick={handle.exit}
+            >
+              {iconNotFull()}
+            </section>
+          ) : (
+            <section
+              className="m-10 rounded-xl bg-white p-2 text-lg transition-colors duration-500 hover:cursor-pointer hover:bg-slate-500 hover:text-white md:text-xl lg:text-3xl"
+              onClick={handle.enter}
+            >
+              {iconFull()}
+            </section>
+          )}
+        </section>
+      </div>
+    </FullScreen>
   );
 }
