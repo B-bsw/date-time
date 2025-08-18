0642d0b9c8d8fde82b46ece24741dd4db48624b6
BB <sorawitthassadorn15@gmail.com>
2025-08-19 00:35:20 +0700

    edit bug

--- src/app/layout.js
+++ src/app/layout.js
@@ -23,7 +23,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} bg-neutral-400 antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} bg-primary antialiased `}
       >
         {children}
       </body>
